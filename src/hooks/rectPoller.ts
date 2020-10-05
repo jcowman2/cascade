d5c704224f72d492c09139ded4fe074bf930adc6
jcowman2 <joe.cowman@huskers.unl.edu>
2020-10-05 10:55:13 -0500

    Wrap rectPoller in useEffect

--- src/hooks/rectPoller.ts
+++ src/hooks/rectPoller.ts
@@ -4,13 +4,18 @@ const useRectPoller = () => {
   const [rect, setRect] = React.useState<DOMRect>();
   const divRef = React.createRef<HTMLDivElement>();
 
-  setInterval(() => {
+  React.useEffect(() => {
     if (!divRef.current) {
       return;
     }
-    const currentRect = divRef.current.getBoundingClientRect();
-    setRect(currentRect);
-  }, 1000);
+    setInterval(() => {
+      if (!divRef.current) {
+        return;
+      }
+      const currentRect = divRef.current.getBoundingClientRect();
+      setRect(currentRect);
+    }, 1000);
+  }, [divRef]);
 
   const top = rect?.top;
   const left = rect?.left;
