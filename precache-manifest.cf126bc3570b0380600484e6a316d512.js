971ceb335b969a5c47601e5196e4d8839477e36a
jcowman2 <joe.cowman@huskers.unl.edu>
2020-10-05 16:24:40 -0500

    Updates

---
+++ precache-manifest.cf126bc3570b0380600484e6a316d512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874ad71bd5b5c6cfac33240e70c3962d",
+    "url": "/cascade/index.html"
+  },
+  {
+    "revision": "0de100791f3ccd834cc3",
+    "url": "/cascade/static/css/main.e1305a31.chunk.css"
+  },
+  {
+    "revision": "f4f249f5907dd08dcefd",
+    "url": "/cascade/static/js/2.c7207953.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cascade/static/js/2.c7207953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de100791f3ccd834cc3",
+    "url": "/cascade/static/js/main.b5449197.chunk.js"
+  },
+  {
+    "revision": "9f2c72f84b52c5bfeb56",
+    "url": "/cascade/static/js/runtime-main.e39f4599.js"
+  }
+]);
